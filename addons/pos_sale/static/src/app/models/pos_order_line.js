Agro-Marin/odoo

[REF] AgroMarin guidelines

addons/pos_sale/static/src/app/models/pos_order_line.js
@@ -41,7 +41,7 @@ patch(PosOrderline.prototype, {
         } else {
             this.setQuantity(
                 saleOrderLine.product_uom_qty -
-                    Math.max(saleOrderLine.qty_delivered, saleOrderLine.qty_invoiced)
+                    Math.max(saleOrderLine.qty_transfered, saleOrderLine.qty_invoiced)
             );
         }
     },

addons/sale_stock_margin/models/sale_order_line.py
@@ -21,7 +21,7 @@ class SaleOrderLine(models.Model):
                     purch_price,
                     product.cost_currency_id,
                 )
-            elif not line.product_uom_qty and line.qty_delivered:
+            elif not line.product_uom_qty and line.qty_transfered:
                 # if line added from delivery and standard price, pass to super
                 line_ids_to_pass.add(line.id)
         return super(SaleOrderLine, self.browse(line_ids_to_pass))._compute_purchase_price()

suniagajose/odoo-marin

addons/pos_sale/static/src/app/models/pos_order_line.js
@@ -41,7 +41,7 @@ patch(PosOrderline.prototype, {
         } else {
             this.setQuantity(
                 saleOrderLine.product_uom_qty -
-                    Math.max(saleOrderLine.qty_delivered, saleOrderLine.qty_invoiced)
+                    Math.max(saleOrderLine.qty_transferred, saleOrderLine.qty_invoiced)
             );
         }
     },

addons/sale_stock_margin/models/sale_order_line.py
@@ -28,7 +28,7 @@ class SaleOrderLine(models.Model):
                     purch_price_uom,
                     product.cost_currency_id,
                 )
-            elif not line.product_uom_qty and line.qty_delivered:
+            elif not line.product_uom_qty and line.qty_transferred:
                 # if line added from delivery and standard price, pass to super
                 line_ids_to_pass.add(line.id)
         return super(SaleOrderLine, self.browse(line_ids_to_pass))._compute_purchase_price()